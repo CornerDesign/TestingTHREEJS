1446148311d9396293cf7d3ef17d7f40c4fa37bf
CornerDesign <50730945+CornerDesign@users.noreply.github.com>
2019-05-17 15:17:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,104 @@
-let scene, renderer, camera
-let cube
+let renderer, scene, camera
 
-// 初始化場景、渲染器、相機、物體
+// 苦力怕物件
+class Creeper {
+  constructor() {
+    // 宣告頭、身體、腳幾何體大小
+    const headGeo = new THREE.BoxGeometry(4, 4, 4)
+    const bodyGeo = new THREE.BoxGeometry(4, 8, 2)
+    const footGeo = new THREE.BoxGeometry(2, 3, 2)
+
+    // 馮氏材質設為綠色
+    const creeperMat = new THREE.MeshPhongMaterial({ color: 0x00ff00 })
+
+    // 頭
+    this.head = new THREE.Mesh(headGeo, creeperMat)
+    this.head.position.set(0, 6, 0)
+
+    // 身體
+    this.body = new THREE.Mesh(bodyGeo, creeperMat)
+    this.body.position.set(0, 0, 0)
+
+    // 四隻腳
+    this.foot1 = new THREE.Mesh(footGeo, creeperMat)
+    this.foot1.position.set(-1, -5.5, 2)
+    this.foot2 = this.foot1.clone() // 剩下三隻腳都複製第一隻的 Mesh
+    this.foot2.position.set(-1, -5.5, -2)
+    this.foot3 = this.foot1.clone()
+    this.foot3.position.set(1, -5.5, 2)
+    this.foot4 = this.foot1.clone()
+    this.foot4.position.set(1, -5.5, -2)
+
+    // 將四隻腳組合為一個 group
+    this.feet = new THREE.Group()
+    this.feet.add(this.foot1)
+    this.feet.add(this.foot2)
+    this.feet.add(this.foot3)
+    this.feet.add(this.foot4)
+
+    // 將頭、身體、腳組合為一個 group
+    this.creeper = new THREE.Group()
+    this.creeper.add(this.head)
+    this.creeper.add(this.body)
+    this.creeper.add(this.feet)
+  }
+}
+
+// 生成苦力怕並加到場景
+function createCreeper() {
+  const creeperObj = new Creeper()
+  scene.add(creeperObj.creeper)
+}
+
+// 畫面初始化
 function init() {
-  // 建立場景
   scene = new THREE.Scene()
 
-  // 建立渲染器
-  renderer = new THREE.WebGLRenderer()
-  renderer.setSize(window.innerWidth, window.innerHeight) // 場景大小
-  renderer.setClearColor(0xeeeeee, 1.0) // 預設背景顏色
-  renderer.shadowMap.enable = true // 陰影效果
-
-  // 將渲染器的 DOM 綁到網頁上
-  document.body.appendChild(renderer.domElement)
-
-  // 建立相機
+  // 相機設定與 OrbitControls
   camera = new THREE.PerspectiveCamera(
-    45,
+    60,
     window.innerWidth / window.innerHeight,
     0.1,
-    100
+    1000
   )
-  camera.position.set(10, 10, 10)
+  camera.position.set(30, 30, 30)
   camera.lookAt(scene.position)
 
-  // 建立光源
-  let pointLight = new THREE.PointLight(0xffffff)
-  pointLight.position.set(10, 10, -10)
-  scene.add(pointLight)
+  // 三軸座標輔助
+  let axes = new THREE.AxesHelper(20)
+  scene.add(axes)
 
-  // 建立物體
-  const geometry = new THREE.BoxGeometry(1, 1, 1) // 幾何體
-  const material = new THREE.MeshPhongMaterial({
-    color: 0x0000ff
-  }) // 材質
-  cube = new THREE.Mesh(geometry, material) // 建立網格物件
-  cube.position.set(0, 0, 0)
-  scene.add(cube)
+  // 渲染器設定
+  renderer = new THREE.WebGLRenderer()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+
+  // 簡單的地板
+  const planeGeometry = new THREE.PlaneGeometry(60, 60)
+  const planeMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff })
+  let plane = new THREE.Mesh(planeGeometry, planeMaterial)
+  plane.rotation.x = -0.5 * Math.PI
+  plane.position.set(0, -7, 0)
+  scene.add(plane)
+
+  // 產生苦力怕物件並加到場景
+  createCreeper()
+
+  // 簡單的 spotlight 照亮物體
+  let spotLight = new THREE.SpotLight(0xffffff)
+  spotLight.position.set(-10, 40, 30)
+  scene.add(spotLight)
+  // let spotHelper = new THREE.SpotLightHelper(spotLight)
+  // scene.add(spotHelper)
+
+  // 將渲染出來的畫面放到網頁上的 DOM
+  document.body.appendChild(renderer.domElement)
 }
 
-// 建立動畫
-function animate() {
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
-}
-
-// 渲染場景
 function render() {
-  animate()
   requestAnimationFrame(render)
   renderer.render(scene, camera)
 }
 
-// 監聽螢幕寬高來做簡單 RWD 設定
 window.addEventListener('resize', function() {
   camera.aspect = window.innerWidth / window.innerHeight
   camera.updateProjectionMatrix()
